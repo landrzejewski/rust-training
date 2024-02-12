45109cdb8db38f1edf02bb637076b50128310f8a
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-02-12 16:41:36 +0100

    Module 1

--- src/main.rs
+++ src/main.rs
@@ -1,120 +1,38 @@
-/* Enums
+use std::cmp::Ordering::{Greater, Less};
+use std::io;
+use rand::Rng;
 
- - reprezentują wyliczenie stałych/możliwych wariantów
- - pozwalają na zdefiniowanie metod (tak samo jak w przypadku struktur)
- */
+fn main() {
+    println!("Guess the number!");
+    let number = rand::thread_rng()
+        .gen_range(1..=10);
 
- use std::mem::size_of_val;
+    loop {
+        println!("Please input number: ");
+        let mut provided_number = String::new();
 
- fn main() -> Result<(), String> {
-     let qr_code: Barcode = Barcode::Qr(String::from("345345345345"));
-     let product_code = Barcode::Product { id: 5, value: String::from("123") };
- 
-     println!("{}", size_of_val(&qr_code));
-     println!("{}", size_of_val(&product_code));
-     println!("{}", size_of_val(&Barcode::Other));
-     //let a  = [qr_code, product_code, Barcode::Other];
- 
-     //println!("Value: {}", Values::from_i32(17));
+        io::stdin()
+            .read_line(&mut provided_number)
+            .expect("Read line failed");
 
-          // Destrukcja elementów wyliczenia
- 
-          match qr_code {
-            Barcode::Other => println!("Other barcode"),
-            Barcode::Product { value, id} => println!("Product {id}:{value} "),
-            Barcode::Qr(value) => println!("Qr {value} "),
-            _ => ()
+        let guess: i32 = match provided_number.trim().parse() {
+            Ok(value) => value,
+            Err(_) => {
+                println!("Incorrect number");
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&number) {
+            Less => println!("Too small"),
+            Greater => println!("Too big"),
+            _ => {
+                println!("You won!");
+                break;
+            }
         }
-    
-        match product_code {
-           Barcode::Product { id: id_value @ 4..=10, value: _ } => println!("Id in big range {id_value}"), // Bindowanie wartości w zakresie
-           Barcode::Product { id: 1..=3, value: _ } => println!("Id in small range"),
-           _ => {}
-        }
- 
-     /* Jednym z wbudowanych typów wyliczenia jest Option reprezentujący wartość lub jej brak (alternatywa dla null)
- 
-     enum Option<T> {
-         None,
-         Some(T),
-     }
-     */
- 
-     let result = safe_div(3.0, 3.0)
-        .expect("Division by 0");
- 
-     match safe_div(3.0, 3.0) {
-         Some(value) => println!("3.0 / 3.0 = {}", value),
-         _ => ()
-     }
- 
-     if let Some(value) = safe_div(3.0, 3.0) {
-         println!("3.0 / 3.0 = {}", value);
-     }
-
-    let Some(value) = safe_div(3.0, 3.0) else {
-        return Err("error".to_string())
-    };
-    println!("3.0 / 3.0 = {}", value);
-
-    let mut stack = vec![1, 2, 3];
-    while let Some(value) = stack.pop() {
-        println!("Value: {}", value);
     }
 
-     /*
-     enum Result<T, E> {
-        Ok(T),
-        Err(E),
-     }
-    */
- 
-     let result = safe_div_with_result(3.0, 3.0)?; // w przypadku Err zwrócenie/wyjście z funkcji
- 
-     match safe_div_with_result(3.0, 3.0) {
-        Ok(value) => println!("3.0 / 3.0 = {}", value),
-        Err(message) => println!("Errro: {message}")
-    }
- 
-    Ok(())
- }
- 
- enum Currency {
-    Eur,
-    Pln,
-    Gbp,
- }
- 
- enum Values {
-    A = 17,
-    B = 42,
-    C
- }
- 
- struct Money {
-    value: f64,
-    currency: Currency,
- }
- 
- #[derive(Debug)]
- enum Barcode {
-    Upc(i32, i32, i32, i32),
-    Qr(String),
-    Product { value: String, id: i64 },
-    Other,
- }
- 
- impl Barcode {
-    fn get_info(&self) -> String {
-        format!("Barcode {:?}", self)
-    }
- }
- 
- fn safe_div(value: f64, dividend: f64) -> Option<f64> {
-    if dividend == 0.0 { None } else { Some(value / dividend) }
- }
- 
- fn safe_div_with_result(value: f64, dividend: f64) -> Result<f64, String> {
-     if dividend == 0.0 { Err("Division by 0".to_string()) } else { Ok(value / dividend) }
- }
- 
+}
