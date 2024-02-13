39226c212b6117ae6b8877598b77f42c742d3e50
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-02-13 13:14:25 +0100

    Module 2

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,17 @@ use std::env;
 
 const SEPARATOR: &str = " ";
 
+fn show_help() {
+    println!("Usage:");
+    println!("cat [args] file1 file2 ...");
+    println!("Args:");
+    println!("  -n - show line numbers");
+    println!("  -nb - show line numbers excluding blank lines");
+}
+
 fn main() {
     let parameters = env::args()
         .skip(1)
         .collect::<Vec<_>>();
-    println!("{}", parameters.join(SEPARATOR));
+
 }
