21053cf8201f288bf821f1c07c15c82ef5a60098
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-02-13 14:17:58 +0100

    Module 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, process::exit};
 
 const SEPARATOR: &str = " ";
 
@@ -10,9 +10,47 @@ fn show_help() {
     println!("  -nb - show line numbers excluding blank lines");
 }
 
-fn main() {
-    let arguments = env::args()
+fn get_arguments() -> Vec<String> {
+    env::args()
+    .skip(1)
+    .collect::<Vec<_>>()
+}
+
+#[derive(PartialEq)]
+enum Mode {
+    
+    WithLineNumbers(bool),
+    WithoutLineNumbers
+
+}
+
+fn get_mode(arguments: &Vec<String>) -> Mode {
+    match arguments[0].as_str() {
+        "-n" => Mode::WithLineNumbers(true),
+        "-nb" => Mode::WithLineNumbers(false),
+        _ => Mode::WithoutLineNumbers
+    }
+}
+
+fn get_file_names(mode: &Mode, arguments: &Vec<String>) -> Vec<String> {
+    if *mode == Mode::WithoutLineNumbers {
+        arguments.clone()
+    } else {
+        arguments.iter()
         .skip(1)
-        .collect::<Vec<_>>();
+        .cloned()
+        .collect()
+    }
+}
+
+fn main() {
+    let arguments = get_arguments();
+    if arguments.is_empty() {
+        show_help();
+        return;
+    }
+    let mode = get_mode(&arguments);
+    let file_names = get_file_names(&mode, &arguments);
+    
 
 }
