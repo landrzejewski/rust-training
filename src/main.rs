df8a348a32b8fc498e12a6c2a31595e6af2ecb26
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-02-14 16:32:21 +0100

    Module 3

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,94 @@
-use std::{collections::HashMap, fs::File, io::{BufRead, BufReader}};
+use std::iter::Iterator;
 
-use lib::get_arguments;
-use walkdir::{DirEntry, WalkDir};
-use utils::{assert, is_not_empty, min_length};
+const FIELD_SIZES: [usize; 5] = [8, 10, 20, 1, 1];
+const EMPTY_VALUE: char = '\0';
 
-mod utils;
-mod lib;
-
-fn show_help() {
-    println!("Usage:");
-    println!("app text path1 path2 ...");
-    println!("Args:");
-    println!("  text - text to find");
+fn i64_to_bytes(value: i64) -> Vec<u8> {
+    value.to_be_bytes().to_vec()
 }
 
-fn get_lines_with_text(text: &String, file_path: &String) -> Vec<String> {
-    let mut lines: Vec<String> = vec![];
-    let Ok(file) = File::open(file_path) else {
-        return Vec::new();
-    };
-    let reader = BufReader::new(file);
-    for (index, line) in reader.lines().enumerate() {
-        if let Ok(current_line) = line {
-            if current_line.contains(text) {
-                lines.push(format!("{:6}:\t{}", index + 1, current_line));
-            }
-        }
+fn i64_from_bytes(bytes: Vec<u8>) -> i64 {
+    let mut bytes_arr = [0; 8];
+    bytes_arr.clone_from_slice(&bytes[0..8]);
+    i64::from_be_bytes(bytes_arr)
+}
+
+fn u8_to_bytes(value: u8) -> Vec<u8> {
+    value.to_be_bytes().to_vec()
+}
+
+fn u8_from_bytes(bytes: [u8; 1]) -> u8 {
+    u8::from_be_bytes(bytes)
+}
+
+fn bool_to_bytes(value: bool) -> Vec<u8> {
+    if value { vec![1u8] } else { vec![0u8] }
+}
+
+fn bool_from_bytes(bytes: [u8; 1]) -> bool {
+    if bytes == [1u8] { true } else { false }
+}
+
+fn string_to_bytes(value: &String, size: usize) -> Vec<u8> {
+    let mut bytes: Vec<u8> = vec![EMPTY_VALUE as u8; size];
+    for (index, byte) in value.clone().into_bytes().iter().enumerate() {
+        bytes[index] = *byte
     }
-    lines
+    bytes
 }
 
-fn grep(text: &String, paths: &Vec<String>) -> HashMap<String, Vec<String>> {
-    let file_filter = |entry: &DirEntry| entry.file_type().is_file();
-
-    let files = |path: &String| WalkDir::new(path)
-        .into_iter()
-        .filter_map(|result| result.ok())
-        .filter(file_filter)
-        .map(|entry| entry.path().display().to_string());
-
-    println!("Searching...");
-    paths.iter()
-        .map(files)
-        .flat_map(|files| files)
-        .map(|path| (path.clone(), get_lines_with_text(text, &path)))
-        .filter(|entry| !entry.1.is_empty())
+fn string_from_bytes(bytes: Vec<u8>) -> String {
+    String::from_utf8(bytes).expect("Found invalid UTF-8")
+        .chars()
+        .filter(|c| *c != EMPTY_VALUE)
         .collect()
 }
 
+#[derive(Debug)]
+struct User {
+    id: i64,
+    first_name: String,
+    last_name: String,
+    is_active: bool,
+    age: u8
+}
+
+fn user_to_record(user: &User) -> Vec<u8> {
+    let first_name_len = user.first_name.len();
+    if first_name_len > FIELD_SIZES[1] {
+        panic!("First name is too big")
+    }
+    if user.last_name.len() > FIELD_SIZES[2] {
+        panic!("Last name is too big")
+    }
+    [
+        i64_to_bytes(user.id),
+        string_to_bytes(&user.first_name, FIELD_SIZES[1]),
+        string_to_bytes(&user.last_name, FIELD_SIZES[2]),
+        bool_to_bytes(user.is_active),
+        u8_to_bytes(user.age)
+    ].concat()
+}
+
+fn record_to_user(bytes: &Vec<u8>) -> User {
+    User {
+        id: i64_from_bytes(bytes[0..8].to_vec()),
+        first_name: string_from_bytes(bytes[8..18].to_vec()),
+        last_name: string_from_bytes(bytes[18..38].to_vec()),
+        is_active: bool_from_bytes([bytes[38]]),
+        age: u8_from_bytes([bytes[39]]),
+    }
+}
+
 fn main() {
-    let arguments = get_arguments();
-    assert(&arguments, min_length(2), show_help);
-
-    let text = &arguments[0].clone();
-    let paths = arguments.iter()
-        .skip(1)
-        .cloned()
-        .collect::<Vec<_>>();
-    assert(&paths, is_not_empty(), show_help);
-
-    for (key, lines) in grep(&text, &paths).into_iter() {
-        println!("{key}");
-        lines.iter().for_each(|line| println!("{line}"));
+    let user = User {
+        id: 1,
+        first_name: String::from("Jan"),
+        last_name: String::from("Kowalski"),
+        is_active: true,
+        age: 18
     };
+
+    let bytes = user_to_record(&user);
+    println!("{:#?}", record_to_user(&bytes));
 }
